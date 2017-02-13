8f69a540bbdec4c8a17c4531cefbdf3211bf5277
nprez <nick_prezioso@hotmail.com>
2017-02-12 20:13:54 -0500

    copied Sesh's stuff

--- src/SongLib/app/SongLib.java
+++ src/SongLib/app/SongLib.java
@@ -3,10 +3,31 @@
 
 package SongLib.app;
 
-public class SongLib {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class SongLib extends Application{
 	private SongLib(){}
 	
-	public static void main(String[] args) {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 		
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/SongLib/view/SongLib.fxml"));
+		
+		GridPane root = (GridPane)loader.load();
+		
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("SongLib");
+		primaryStage.setResizable(false);  
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
